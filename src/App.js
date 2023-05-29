e0b9b6503d793ec099afa85d9027d92765226a23
Hector Hinojosa <hectorahinojosa@gmail.com>
2023-05-29 00:33:11 -0400

    Added Boot strap and fixed some of the Regex. Added links to common books used.

--- src/App.js
+++ src/App.js
@@ -2,30 +2,27 @@ import React, { useState } from 'react';
 import './App.css';
 import axios from "axios";
 
+// layout import 
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+
 
 // Regular Expressions
 var filterVTTLink = /(https:?:[a-zA-Z0-9._/-]*\.vtt)/; // Removes the link to the VTT file from the pubmedialinks api response
-var filterFormatting1 = /WEBVTT/g; // Removes WEBVTT off from the VTT Data
-var filterFormatting2 = /\d+:\d+:\d+.\d+\s-->\s\d+:\d+:\d+.\d+\s\S*\s\S*\s*align:center/gm; // Removes time codes and some formatting
-var filterFormatting3 = /\\r\\n\\r\\n \\r\\n/g; // Removes the final carriage returns and new lines
-var filterFormatting4 = /\\r\\n/g; // Removes the final carriage returns and new lines
-// var filterPub = /pub=([a-zA-Z]*).*[^track].*/gm;
-// var filterPubTrack = /pub=(.*)&track=(\d+)/gm;
-// var filterDocID = /docid=(\d+)/gm;
-// var filterJWB = /(pub[a-zA-Z0-9-_]*)/gm;
-// var filterPubMediaLinks = /GETPUBMEDIALINKS/g;
-// var filterMediator = /mediator/g;
+var filterFormatting1 = /"WEBVTT/g; // Removes WEBVTT off from the VTT Data
+var filterFormatting2 = /([\d]+:[\d]+){1,}(\.[\d]+)/g;
+var filterFormatting3 = /(\s-->\s)|(line[.:,0-9%\sa-z]+)|(\\r\\n‎)/g;
+var filterFormatting4 = /(\\r\\n){1,}|(\\r\\n)\s{1,}|(\\n){1,}/g;
+var filterFormatting5 = /\n\s{2,}/g;
+var filterFormatting6 = /<[/a-zA-z]+>|[\s]{2,}/g;
 var filterRemoveLank = /lank=(.*)&/;
-// var testDocID = /docid/gm;
-// var testURL = /^(?:([A-Za-z]+):)?(\/{0,3})([0-9.\-A-Za-z]+)(?::(\d+))?(?:\/([^?#]*))?(?:\?([^#]*))?(?:#(.*))?$/g;
 
 function App({initialValue}) {
   const [VTTLink, setVTTLink] = useState(false);
-  const [videoTitle, setvideoTitle] = useState(false);
+  const [videoTitle, setVideoTitle] = useState(false);
   const [VTTData, setVTTData] = useState(false);
   const [inputURL, setinputURL] = useState('');
   const [error, setError] = useState('');
-  // const [pubMediaLinksData , setPubMediaLinksData] = useState('');
 
   function filterPubFromLink(inputURL){
     let groups = inputURL.match(filterRemoveLank);
@@ -52,9 +49,13 @@ function App({initialValue}) {
   }
 
   function filterVTTLinkFromMediatorResponse(json){
+
     let filteredResults = filterVTTLink.exec(json);
     if(filteredResults){
-      getVTTFileFromJWServer(filteredResults[0]);      
+      let parsedJson = JSON.parse(json);
+      setVideoTitle(parsedJson.media[0].title);
+      getVTTFileFromJWServer(filteredResults[0]);     
+      // console.log(json); 
     }
     else{
       setError('Your link does not seem to be working');
@@ -65,7 +66,9 @@ function App({initialValue}) {
     axios
       .get(link)
       .then((response) => {
+
         let stringifiedResponse = JSON.stringify(response.data);
+        // setVTTData(stringifiedResponse);
         filterFormattingFromVTTFile(stringifiedResponse);
       })
       .catch(error => {
@@ -75,12 +78,13 @@ function App({initialValue}) {
 
   function filterFormattingFromVTTFile(VTTData){
     let nothing = ' ';
-    let filteredResults = VTTData.replace(filterFormatting1,nothing);               //removing the WEBVTT at the front of the paragraph
+    let filteredResults = VTTData.replace(filterFormatting1,nothing);               //removing the "WEBVTT at the front of the paragraph
     let secondFilteredResults = filteredResults.replace(filterFormatting2,nothing); // removing the timecodes
-    let thirdFilteredResults = secondFilteredResults.replace(filterFormatting3, '\r'); // remove the \r and \n
-    let fourthFilteredResults = thirdFilteredResults.replace(filterFormatting4, nothing) 
-    let final = fourthFilteredResults.substring(1,fourthFilteredResults.length-1);
-    setVTTData(final.trim());
+    let thirdFilteredResults = secondFilteredResults.replace(filterFormatting3,nothing); // remove the \r and \n
+    let fourthFilteredResults = thirdFilteredResults.replace(filterFormatting4, '\n'); 
+    let fifthFilteredResults = fourthFilteredResults.replace(filterFormatting5,'\n');
+    let sixthFilteredResults = fifthFilteredResults.replace(filterFormatting6,nothing);
+    setVTTData(sixthFilteredResults);
   }
 
   // handleSubmit function
@@ -88,7 +92,7 @@ function App({initialValue}) {
     setError('');
     setVTTData('');
     setVTTLink('');
-    setvideoTitle('');
+    setVideoTitle('');
     event.preventDefault();
     // check that it is in fact a url then below
     filterPubFromLink(inputURL);
@@ -96,29 +100,42 @@ function App({initialValue}) {
 
 
   return(
-    <div className="wrapper">
-      <h1>JW.Org Video Captions Extractor</h1>
 
-      <form onSubmit={handleSubmit}>
-        <fieldset>
-          <label>
-            <p>Right Click a video on JW.ORG-> LFF and paste the link here to get captions</p>
-            <input type="text" size="70" value={inputURL} onChange={e=>setinputURL(e.target.value)}/>
-          </label>
-        </fieldset>
-        <button type="submit">Begin Extraction</button>
-      </form>
+    <Container className="p-3">
+      <Container className="p-5 mb-4 bg-light rounded-3">
+        <h1 className="d-flex justify-content-center">JW.Org Video Captions Extractor</h1>
+        <div className="wrapper">
+          <form onSubmit={handleSubmit}>
+            <fieldset>
+              <label>
+                <p className="d-flex justify-content-center">Right Click a video on JW.ORG-> LFF and paste the link here to get captions</p>
+                <input type="text" size="70" value={inputURL} onChange={e=>setinputURL(e.target.value)} className="d-flex justify-content-center"/>
+              </label>
+            </fieldset>
+            <Button className="d-flex justify-content-center" type="submit">Begin Extraction</Button>
+            <hr/>
+          </form>
 
-      <div className="error">{error}</div>
-      <br/>      
-      <div className="videoTitle">{videoTitle}</div>
-      <br/>
-      <div className="VTTLink">{VTTLink}</div>
-      <br/>
-      <div className="VTTData">{VTTData && <textarea rows="30" cols="100" value={VTTData}/>}
-      </div>
-       
-    </div>
+          <div className="error">{error}</div>
+          <br/>
+          <div className="videoTitle"><h3>Title:</h3> {videoTitle}</div>
+          <br/>
+          <div className="VTTData"><h3>Video Closed Captions:</h3>{VTTData && <textarea className="maxWidth maxHeight" defaultValue={VTTData}/>}
+          <br/>
+          <div className="VTTLink">{VTTLink}</div>
+          </div>
+          <br/>
+          <div className="information"><h3> Check out some of the already prepared publications we use normally</h3></div>
+          <div className="links">
+            <ul>
+              <li><a href="">LFF - Live Forever Book - Normal Print</a></li>
+              <li><a href="">LFF - Live Forever Book - Large Print</a></li>
+              <li><a href="">LFF - Live Forever Book - Largre Print - Main Point Highlighted</a></li>
+            </ul>
+        </div>
+      </div>  
+      </Container>
+    </Container>
     )
 }
 
